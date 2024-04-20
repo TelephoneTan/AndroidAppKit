30f235c5b526e15c0dec8630c67c0d9719f3c46a
Telephone <telephone.tan@qq.com>
2024-04-21 07:09:39 +0800

    添加模块

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 pluginManagement {
     repositories {
         google {
@@ -11,14 +13,31 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+//
+val properties = Properties()
+val propertiesFile = File(rootDir, "project.local.properties")
+if (propertiesFile.exists()) {
+    propertiesFile.inputStream().use { properties.load(it) }
+}
+//
+val githubUser: String? = properties.getProperty("gpr.github.user")
+val githubKey: String? = properties.getProperty("gpr.github.key")
+//
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/TelephoneTan/JavaHTTPRequest")
+            credentials {
+                username = githubUser ?: System.getenv("GITHUB_USERNAME")
+                password = githubKey ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
 
 rootProject.name = "AppKit"
 include(":app")
- 
+include(":appKit")
