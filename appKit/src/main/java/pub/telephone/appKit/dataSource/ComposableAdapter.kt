46ddb9a9ca204c7a683b1029f4717c10504fc157
Telephone <telephone.tan@qq.com>
2024-05-04 18:59:20 +0800

    新增 ComposableAdapter 用于支持在 Compose 场景中使用 DataAdapter

---
+++ appKit/src/main/java/pub/telephone/appKit/dataSource/ComposableAdapter.kt
@@ -0,0 +1,59 @@
+package pub.telephone.appKit.dataSource
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.viewbinding.ViewBinding
+import pub.telephone.appKit.dataSource.DataSource.DataSourceParameters
+
+abstract class ComposableAdapter<D, CD : ComposableNode>(
+    params: DataSourceParameters.State,
+    private val srcState: State<List<D>>
+) : DataAdapter<DataViewHolder<ViewBinding>, CD> {
+    @Suppress("LeakingThis")
+    final override val source = DataSource(DataSourceParameters(params), this)
+
+    private val state: MutableState<DataNode.Result<List<CD>>> =
+        mutableStateOf(DataNode.Result.Init())
+
+    private fun releaseState() {
+        state.value = DataNode.Result.Succeed(source.GetAll())
+    }
+
+    final override fun notifyItemRangeInserted(positionStart: Int, itemCount: Int) {
+        releaseState()
+    }
+
+    final override fun notifyItemRemoved(position: Int) {
+        releaseState()
+    }
+
+    final override fun notifyItemChanged(position: Int, payload: Any?) {
+        // Compose 使用场景下不支持刷新指定子项
+        // do nothing
+    }
+
+    final override fun notifyDataSetChanged() {
+        releaseState()
+    }
+
+    @Composable
+    @SuppressLint("ComposableNaming")
+    protected abstract fun __Content__(list: DataNode.Result<List<CD>>)
+
+    protected abstract fun map(data: D): CD
+
+    @Composable
+    fun Content() {
+        remember {
+            derivedStateOf {
+                source.ShuffleAll({ _ -> srcState.value.map(::map) })
+                true
+            }
+        }
+        __Content__(state.value)
+    }
+}
