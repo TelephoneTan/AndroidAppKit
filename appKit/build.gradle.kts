78a8be4eeba2bdda2d57c7d63baec3126f0ba65e
Telephone <telephone.tan@qq.com>
2024-05-05 19:01:46 +0800

    v10.0.0

--- appKit/build.gradle.kts
+++ appKit/build.gradle.kts
@@ -70,7 +70,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "pub.telephone"
             artifactId = "app-kit"
-            version = "9.2.4"
+            version = "10.0.0"
 
             afterEvaluate {
                 from(components["release"])
